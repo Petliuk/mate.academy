4740b11463d06b48d163523b784fec86d6596fa8
Maks <petliukofficial@gmail.com>
2024-12-24 22:38:51 +0200

    Added dto and controller and all necessary objects

--- src/main/java/com/example/mate/service/BookService.java
+++ src/main/java/com/example/mate/service/BookService.java
@@ -1,11 +1,15 @@
 package com.example.mate.service;
 
-import com.example.mate.model.Book;
+import com.example.mate.dto.BookDto;
+import com.example.mate.dto.CreateBookRequestDto;
 import java.util.List;
 
 public interface BookService {
 
-    List<Book> getAllBooks();
+    List<BookDto> getAll();
 
-    Book addBook(Book book);
+    BookDto getBookById(Long id);
+
+    BookDto createBook(CreateBookRequestDto bookDto);
+
 }
